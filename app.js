e9cba3eb3f6c078b8f402ab56b808589993a1dd7
Keyen331 <banayos.kay@gmail.com>
2017-03-15 08:56:22 -1000

    adding

---
+++ app.js
@@ -0,0 +1,21 @@
+var JSONdataHeader = [
+  {
+  "header" : "h1", "h4", "img" 
+  },
+];
+
+varJSONbody = [
+  {
+    "div" : [{ "class" : "class", "h2", }];
+  }
+];
+
+var simplifyJson = [
+  {
+    "journalEntry" : "1",
+    "date" : "March 15, 2017",
+    "title" : "Journ",
+    "content" : "The reason why I took this class is with the goal to learn basics way on how computer works. As a world that is dependent on technology, I must obtain a little knowledge of the methods that we rely on in almost everyday. It is important to learn how we can command computers because we will be able to make it do what we expect and desire as its function. Having the control on your computer will enable you to obtain some knowledge or skills that could help yourself from becoming a victim of today's danger in technology use.",
+    "img" : undefined
+  }
+];
