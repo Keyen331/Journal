ce188fedf03670fd94c92e5fe0d497ca8b5b3b48
Keyen331 <banayos.kay@gmail.com>
2017-03-17 09:16:20 -1000

    adding

---
+++ pseudoCode.js
@@ -0,0 +1,21 @@
+/* 
+  create a function named showAll
+  Pass one parameter (data)
+
+  Inside the function
+  Iterate through data
+    for each object create element 
+    create parent div 
+      with the id/class '#container-' + object.id
+    create a h1
+      append/add inner.HTML to h1
+    Repeat for every value
+    append all elements + values to the parent div
+*/
+
+function showAll(data) {
+  for (var key in data) {
+    
+  }
+  }
+}
